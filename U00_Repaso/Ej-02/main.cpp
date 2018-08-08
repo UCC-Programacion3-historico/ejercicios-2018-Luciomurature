5c86ced0d4efcf08e1c5e8bb50252cfb084a7bc8
luciomurature <luciomurature@gmail.com>
2018-08-08 10:14:45 +0000

    calculadora templates

--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 int main() {
-    cout << "Ejercicio 00/02\n" << endl;
+    Calculadora<int> micalc;
+    Calculadora<double> micalcDouble;
+    cout << micalc.sumar(2,2)<< endl;
+    cout << micalc.restar(2,3) << endl;
+    cout << micalc.dividir(15,3) << endl;
+    cout << micalc.multiplicar(3,8) << endl;
+    cout << micalcDouble.sumar(0.258963,2.528)<< endl;
+    cout << micalcDouble.restar(2.5,3.89665874) << endl;
+    cout << micalcDouble.dividir(15,3) << endl;
+    cout << micalcDouble.multiplicar(5.36,8.98) << endl;
+
+
     return 0;
 }
