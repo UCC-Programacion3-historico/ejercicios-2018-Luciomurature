e0eae0014166329c4a3997d4112ff9ccc0ca6de3
luciomurature <luciomurature@gmail.com>
2018-08-15 11:08:49 +0000

    numeros espaciados

--- U01_Recursividad/Ej-05/digitos.cpp
+++ U01_Recursividad/Ej-05/digitos.cpp
@@ -4,14 +4,33 @@
 using namespace std;
 
 unsigned int cociente(unsigned int a, unsigned int b) {
-    return 0;
+    if(b > a)
+        return 0;
+    else
+        return cociente(a-b, b) + 1;
 }
 
 
 unsigned int resto(unsigned int a, unsigned int b) {
-    return 0;
+    if(a<b){
+        return a;
+    }
+    else{
+        return resto(a-b, b);
+    }
 }
 
 
 void escribir_espaciado(unsigned int num) {
+    int var;
+    if(num < 10){
+        std::cout << num << "  ";
+        return;
+    }
+    else{
+        var = resto(num, 10);
+        num = cociente(num, 10);
+        escribir_espaciado(num);
+        std::cout << var << "  ";
+    }
 }
--- U01_Recursividad/Ej-05/main.cpp
+++ U01_Recursividad/Ej-05/main.cpp
@@ -2,6 +2,6 @@
 #include "digitos.h"
 
 int main() {
-    std::cout << "Ejercicio 01/05\n" << std::endl;
+    escribir_espaciado(1024);
     return 0;
 }
